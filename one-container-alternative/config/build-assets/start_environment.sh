bde8e6c37a12fd87c2c9962de19cb37d37a0e7a9
Jesus Illescas <jillesca@example.com>
2024-10-23 17:15:08 +0100

    setting env vars on dockerfile

--- one-container-alternative/config/build-assets/start_environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# starts yangsuite, mimics behavior from yangsuite docker-compose 
-
-source /build-assets/setup.env
-
-echo "Starting migrate_and_start.sh..."
-/build-assets/migrate_and_start.sh
-echo "migrate_and_start.sh started with exit code $?"
